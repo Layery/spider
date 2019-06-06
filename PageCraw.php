f5e5abe82fccdc28bb28da537534570b7f504224
layery <weidingyi@aliyun.com>
2019-06-06 11:25:10 +0800

    update

--- PageCraw.php
+++ PageCraw.php
@@ -28,8 +28,8 @@ if ($crawParams == 'web') {
     $baseUrl = 'http://private70.ghuws.win/thread0806.php?fid=22';
 } else if ($crawParams == 'img') {
     $baseUrl = 'http://private70.ghuws.win/thread0806.php?fid=8';
-    $baseUrl = 'http://private70.ghuws.win/thread0806.php?fid=7'; // 技术讨论
     $baseUrl = 'http://private70.ghuws.win/thread0806.php?fid=16';
+    $baseUrl = 'http://private70.ghuws.win/thread0806.php?fid=7'; // 技术讨论
 }
 //http://cl.wpio.xyz/htm_mob/22/1903/3469154.html
 $hostInfo = pathinfo($baseUrl);
@@ -62,7 +62,6 @@ $spider->setHeader([
         'Cookie' => 'ismob=1; hiddenface=; cssNight=; __cfduid=d9195a93c4a594e3459abb8c62987d9a21558925417; PHPSESSID=sb9ls5v5l3ou823hc24t8m9aj1; UM_distinctid=16aff16dd8511c-0a45c2d4e2a94b-52504913-49a10-16aff16dd8b1d6; 227c9_lastvisit=0%091559626523%09%2Fread.php%3Ftid%3D3542890; CNZZDATA950900=cnzz_eid%3D254784805-1559056130-%26ntime%3D1559628669'
     ]);
 
-
 $crawParams = 'img';
 
 if ($crawParams == 'img') {
@@ -70,8 +69,9 @@ if ($crawParams == 'img') {
     for ($i = $loopStart; $i <= $loopEnd; $i++) {
         $url = $baseUrl . '&page=' . $i;
         logWrite('begin catching the ' . $i . ' page');
-        $html = $spider->setUrl($url)
-                ->get();
+//        $html = $spider->setUrl($url)
+//                ->get();
+        $html = file_get_contents('./debug.html');
         $crawler = new Crawler($html);
         $postTitle = $crawler->filterXPath('//title')->text();
         try {
@@ -83,11 +83,12 @@ if ($crawParams == 'img') {
                     if (strpos($title, '↑') !== false || strpos($title, '■■■') !== false) {
                         continue;
                     }
-                    if (!preg_match("/\(\d+[pP]\)|\[\d+[pP]\]|［\d+[pP]］/", $title)) { // @ todo 不存在[p]的干掉
+                    if (!preg_match("/\(\d+[pP]\)|\[\d+[pP]\]|［\d+[pP]］|图|精品/is", $title)) { // @ todo 不存在[p]的干掉
                         continue;
                     }
                     preg_match('/\[.*?\].*?\[\d+[pP]\]/is', $title, $m);
                     $title = $m[0];
+
                     $href = str_replace(["'", ";"], "", $node->getAttribute('onclick'));
                     $href = substr($href, 16);
                     $href = $hostInfo['dirname'] . '/'. $href;
@@ -97,6 +98,8 @@ if ($crawParams == 'img') {
                     ];
                 }
             }
+            file_put_contents('./debug', implode("\n", $temp));
+            die;
             $totalItem = count($data);
             logWrite('parse the ' . $i . ' page success count ' . $totalItem . ' item');
             if (!empty($data)) {
--- baidu.craw.php
+++ baidu.craw.php
@@ -13,7 +13,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 $url = 'https://www.yinduowang.com/index/ajaxsignin';
 $url = 'https://kkembed.kdwshell.com/get_file/3/a7b22583cac2638c053858d31ea5346a/61000/61532/61532.mp4';
-$url = 'http://private70.ghuws.win/thread0806.php?fid=22';
+$url = 'https://wannianrili.51240.com/ajax/?q=2019-08&v=19021213';
 $crawRootPath = './crawFiles/yinduo/';
 
 set_time_limit(0); // script keep running;
@@ -21,23 +21,21 @@ set_time_limit(0); // script keep running;
 $spider = new Spider();
 $rs = $spider->setUrl($url)
              ->setHeader([
-                 'Host' => 'private70.ghuws.win',
-                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                 'Upgrade-Insecure-Requests' => '1',
-                 #'accept-encoding' => 'gzip, deflate, br', // 发送编码之后的数据
+                 ':authority' => 'wannianrili.51240.com',
+                 ':method' => 'GET',
+                 ':path' => '/',
+                 ':scheme' => 'https',
+                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
                  'accept-language' => 'zh-CN,zh;q=0.9',
                  'cache-control' => 'no-cache',
+                 'cookie' => 'Hm_lvt_fbe0e02a7ffde424814bef2f6c9d36eb=1559730643; Hm_lpvt_fbe0e02a7ffde424814bef2f6c9d36eb=1559730841',
                  'pragma' => 'no-cache',
-                 'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8',
                  'upgrade-insecure-requests' => '1',
-                 'referer' => 'https://hs.etet.men/index.php',
-                 'user-agent' => 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Mobile Safari/537.36',
-                 'Cookie' => 'ismob=1; hiddenface=; cssNight=; __cfduid=d9195a93c4a594e3459abb8c62987d9a21558925417; PHPSESSID=sb9ls5v5l3ou823hc24t8m9aj1; UM_distinctid=16aff16dd8511c-0a45c2d4e2a94b-52504913-49a10-16aff16dd8b1d6; 227c9_lastvisit=0%091559626523%09%2Fread.php%3Ftid%3D3542890; CNZZDATA950900=cnzz_eid%3D254784805-1559056130-%26ntime%3D1559628669'
+                 'user-agent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
              ])
              ->post();
 
-
-p($rs);
+exit($rs);
 
 
 
