0ae2da5aa945b88bd68e9cd9f844ff7701f93d4d
layery <weidingyi@aliyun.com>
2019-09-30 18:36:44 +0800

    update pagecraw

--- PageCraw.php
+++ PageCraw.php
@@ -44,6 +44,102 @@ $spider->setHeader([
         'Cookie' => 'ismob=1; hiddenface=; cssNight=; __cfduid=d9195a93c4a594e3459abb8c62987d9a21558925417; PHPSESSID=sb9ls5v5l3ou823hc24t8m9aj1; UM_distinctid=16aff16dd8511c-0a45c2d4e2a94b-52504913-49a10-16aff16dd8b1d6; 227c9_lastvisit=0%091559626523%09%2Fread.php%3Ftid%3D3542890; CNZZDATA950900=cnzz_eid%3D254784805-1559056130-%26ntime%3D1559628669'
 ]);
 
+if ($crawParams == 'fanyi') {
+    $url = 'https://fanyi.baidu.com/v2transapi?from=en&to=zh';
+    $rs = $spider->setUrl($url)
+        ->setHeader([
+            ':authority' => 'fanyi.baidu.com',
+            ':method' => 'POST',
+            ':path' => '/v2transapi?from=en&to=zh',
+            ':scheme' => 'https',
+            'accept' => '*/*',
+//            'accept-encoding' => 'gzip, deflate, br',
+            'accept-language' => 'zh-CN,zh;q=0.9',
+            'content-length' => '128',
+            'content-type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+            'cookie' => 'BIDUPSID=B89ACDE89A40C5001BF6257737FBB1FB; PSTM=1567674158; BAIDUID=33CAECC0F0551EDDC8D3B46AA3D0585C:FG=1; H_WISE_SIDS=130610_126887_136263_100807_134724_135927_132550_134982_120161_133982_136366_132909_136456_136617_131246_136683_136722_132378_131517_118897_118871_118852_118821_118796_107316_132785_136800_136431_136092_133351_129651_136193_132250_128967_135307_133847_132551_135433_135873_134047_136602_131423_133858_135859_134317_136018_110085_136142_134155_127969_131951_135624_136614_135457_127416_136078_136302_136635_136691_133863_134844_136322_136414_100458; H_PS_PSSID=1464_21092_29522_29721_29568_29220; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; delPer=0; BCLID=11750445540464858929; BDSFRCVID=e50OJeC626U4nyvwuMdCKwLlTFNWO83TH6aoYfCpmNHguzGYCYbMEG0PeU8g0KAbWimyogKK0mOTHv-F_2uxOjjg8UtVJeC6EG0Ptf8g0M5; H_BDCLCKID_SF=tJCJ_KtKJID3qR5gMJ5q-n3HKUrL5t_XbI6y3JjOHJOoDDvNjj3cy4LdjGKqL63XM6cfLl6e0hvb8bR2QxTRXTvy3-Aq54Rk5e7jWlrd5-50sUTJWt5YQfbQ0MoPqP-jW26a-bTEMR7JOpkxhfnxyb5DQRPH-Rv92DQMVU52QqcqEIQHQT3m5-5bbN3ut6T2-DA__I_5JI3P; MCITY=-131%3A; PSINO=2; ZD_ENTRY=baidu; locale=zh; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1569832125; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1569832125; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; __yjsv5_shitong=1.0_7_5ecfe819746873d8b9b8d983dcf9e1163a69_300_1569832126409_111.198.24.176_e21d2902; yjs_js_security_passport=568ebfd9b492cba8a3683d4bac0c5d1b891dc879_1569832126_js; from_lang_often=%5B%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D',
+            'origin' => 'https://fanyi.baidu.com',
+            'referer' => 'https://fanyi.baidu.com/',
+            'user-agent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+            'x-requested-with' => 'XMLHttpRequest',
+        ])
+//        ->setReturnCharset()
+        ->post([
+            'from' => 'en',
+            'to' => 'zh',
+            'query' => 'have nice dream',
+            'transtype' => 'translang',
+            'simple_means_flag' => '3',
+            'sign' => '930695.708791',
+            'token' => '6526868664eef31a39c95d071e9c1da9',
+        ]);
+
+
+    exit($rs);
+}
+
+if ($crawParams == '91') {
+//    https://91dizhi-at-gmail-com-0531.w24.rocks/index.php
+    $baseUrl = 'https://91dizhi-at-gmail-com-0531.w24.rocks/v.php?category=hot&viewtype=basic&page=2';
+    $host = parse_url($baseUrl);
+    $host = $host['scheme'] . '://'. $host['host'];
+
+    for ($i = $loopStart; $i <= $loopEnd; $i++) {
+        $url = $host . '/v.php?category=hot&viewtype=basic&page=' . $i;
+//        $html = file_get_contents($runTimePath. 'debug.html');
+        $html = file_get_contents($url);
+        $crawler = new Crawler($html);
+        $itemList = $crawler->filterXPath('//div[@class="listchannel"]/a');
+        foreach ($itemList as $key => $node) {
+            $title = $node->getAttribute('title');
+            if (IS_WIN) {
+                $title = mb_convert_encoding($title, 'gbk');
+            }
+            $href = $node->getAttribute('href');
+            $data[] = [
+                'href' => $href,
+                'title' => $title
+            ];
+        }
+        logWrite('craw the '. $i . ' page count '. count($data). ' item');
+    }
+
+    if (!empty($data)) {
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+        $htmlPath = $htmlStr = '';
+        $crawler = null;
+        $dictStr = '';
+        try {
+            foreach ($data as $key => $row) {
+                $html = file_get_contents($row['href']);
+
+                $crawler = new Crawler($html);
+                $sourceTitle = $row['title'];
+                exit($html);
+                p($crawler->filterXPath('//textarea[@class="fullboxtext"]')->count());
+                $sourceLink = $crawler->filterXPath('//textarea[@class="fullboxtext"]')->eq(0)->text();
+                p($sourceLink);
+                $dictStr .= '<tr>';
+                $dictStr .= '<td class="td-id"><span class="num">' . ($key + 1) . '</span></td>';
+                $dictStr .= '<td class="td-title"><a href="' . $sourceLink . '" target="_bank">' . $sourceTitle . '</a></td>';
+                $dictStr .= '</tr>';
+            }
+
+        } catch (\Exception $e) {
+
+        }
+
+        exit($dictStr);
+
+        $dictStr = preg_replace('/\<\{CONTENT\}\>/s', $dictStr, $tplFile);
+        if (!is_dir($saveDictPath)) {
+            mkdir($saveDictPath, 0777, true);
+        }
+        file_put_contents($saveDictPath . 'search' . '.html', $dictStr);
+        logWrite('save the search dict success');
+    }
+}
 
 if ($crawParams == 'fed') {
     $baseUrl = 'https://www.ffvan.com/video/index/cid/10';
--- baidu.craw.php
+++ baidu.craw.php
@@ -16,58 +16,29 @@ $url = 'https://dw27.malavida.com/dwn/8573ff0bfe7d80aa1153d941614cc62f4ba0bf52cb
 $url = 'http://packagess.deepin.com:8088/releases/15.10.1/deepin-15.10.1-amd64.iso';
 $url = 'https://ffvan.com/video/index/cid/10';
 $url = 'https://ffvan.com/video/show/id/67389';
-$url = 'https://www.ffvan.com/video/show/id/68845';
 $url = 'http://wx567336e2a104442f.wxa.dzsaascdn.com/creeper/Vote';
+$url = 'https://www.ffvan.com/video/show/id/68845';
+$url = 'http://www.baidu.com';
 set_time_limit(0); // script keep running;
 
 $spider = new Spider();
 
-$str = "http://wx567336e2a104442f.wxa.dzsaascdn.com/creeper/index?aid=wx567336e2a104442f&cpid=00d9ae25a1004b4da3de2256bfaf06d4&optionid=46f4727b62bd4c849cff34266be28388&appid=wx567336e2a104442f";
 
-$params = [
-    'cpid' => '00d9ae25a1004b4da3de2256bfaf06d4',
-    'optionid' => '46f4727b62bd4c849cff34266be28388',
-    'openid' => 'oRgIb6AAcH2cBCLjbfZf9KwyZaKM',
-    'latitude' => '',
-    'longitude' => ''
-];
-
-
-
-function getOpenID()
+function getOpenID($len = 10)
 {
     $str = 'abcdefghigjklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789';
     $randStr = '';
-    for ($i = 0, $cnt = 10; $i < $cnt; $i++) {
-        $randStr .= $str{mt_rand(0, 64)};
+    for ($i = 0, $cnt = $len; $i < $cnt; $i++) {
+        $randStr .= $str{mt_rand(0, (strlen($str)-1))};
     }
-    return $randStr;
+    return str_shuffle($randStr);
 }
 
-$spider->setUrl($url)
-    ->setHeader([
-        'Host' => 'wx567336e2a104442f.wxa.dzsaascdn.com',
-        'Connection' => 'keep-alive',
-        'Content-Length' => '115',
-        'Accept' => '*/*',
-        'Origin' => 'http://wx567336e2a104442f.wxa.dzsaascdn.com',
-        'X-Requested-With' => 'XMLHttpRequest',
-        'User-Agent' => 'Mozilla/5.0 (Linux; Android 7.0; SM-G9200 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044904 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x26070333) NetType/4G Language/zh_CN Process/tools',
-        'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Referer' => 'http://wx567336e2a104442f.wxa.dzsaascdn.com/creeper/index?aid=wx567336e2a104442f&cpid=00d9ae25a1004b4da3de2256bfaf06d4&optionid=46f4727b62bd4c849cff34266be28388',
-        'Accept-Encoding' => 'gzip, deflate',
-        'Accept-Language' => 'zh-CN,zh-CN;q=0.9,en-US;q=0.8',
-//        'Cookie' => 'ASP.NET_SessionId=h2cdtcklz0dnav5ifeek4uug; Hm_lvt_917473e977996fda4a9af93570a7464c=1568802621; Hm_lpvt_917473e977996fda4a9af93570a7464c=1568811146',
-    ]);
-while (true) {
-    $params['openid'] = getOpenID();
+$html = $spider->setUrl($url)
+    ->setHeader()
+    ->get(['tt' => 'debug']);
 
-
-    sleep(mt_rand(1, 5));
-}
-
-
-
+p($html);
 
 
 
